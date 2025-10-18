7c0c797ef44214277ba09f0577fece62153772c9
Kiranvc15 <kiranvc15@gmail.com>
2025-10-18 12:57:12 +0530

    Removed unnecessary files from the circleci and added emqx service to the pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,24 +3,30 @@ module.exports = {
     {
       name: "manager-api",
       script: "mvn",
-      args: "spring-boot:run",
+      args: "spring-boot:run -Dspring-boot.run.profiles=dev",
       cwd: "/root/xiaozhi-esp32-server/main/manager-api",
-      interpreter: "none"   // important, tells PM2 not to use node/python
+      interpreter: "none"
     },
-      {
+    {
       name: "manager-web",
       script: "npm",
       args: "run serve",
       cwd: "/root/xiaozhi-esp32-server/main/manager-web",
       interpreter: "none"
     },
- 
     {
       name: "mqtt-gateway",
       script: "app.js",
       cwd: "/root/xiaozhi-esp32-server/main/mqtt-gateway",
       interpreter: "node",
       watch: true
+    },
+    {
+      name: "livekit-server",
+      script: "python",
+      args: "main.py dev",
+      cwd: "/root/xiaozhi-esp32-server/main/livekit-server",
+      interpreter: "none"
     }
   ]
 };
